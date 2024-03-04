f2d95d594b0b803c160858735e0c9da2a1a77045
machulsky61 <83562974+machulsky61@users.noreply.github.com>
2024-03-04 17:04:10 -0300

    3er entrega labo

---
+++ exercises 3/rotarstr.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string rotar(string s, int j) {
+
+    string v;
+    int k = s.size() - (j % s.size());
+    
+    for(int i = k; i < s.size(); i++){
+        v.push_back(s[i]);
+    }
+    for(int i = 0; i < k; i++){
+        v.push_back(s[i]);
+    }
+    
+
+    return v;
+}
+
+int main()
+{
+    /* No hace falta modificar el main */
+    // Leo la entrada
+    string s;
+    int j;
+    cin >> s >> j;
+    
+    string res = rotar(s, j);
+    cout << res;
+    return 0;
+}
---
+++ exercises 3/subseq.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool subsecuencia(string s, string t) {
+    /* existen indices i0 < i1 < i2 tales que s[0] == t[i0] , s[1] == t[i1] , s[2] == t[i2] */
+    
+    int j0 = 0;
+    int res = 0;
+    
+    for(int i = 0; i < s.size(); i++){
+        
+        for(int j = 0; j < t.size(); j++){
+            
+            if(j0 <= j && s[i] == t[j]){
+                
+                res = res + 1;
+                j0 = j;
+                
+                
+            }
+            
+        }
+        
+        
+        
+    }
+    return res == s.size();
+}
+
+int main()
+{
+    /* No hace falta modificar el main */
+    // Leo la entrada
+    string s, t;
+    cin >> s >> t;
+    
+    bool res = subsecuencia(s, t);
+    cout << (res ? "true" : "false");
+    return 0;
+}
+
